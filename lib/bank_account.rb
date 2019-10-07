b76a71a9e54bf0cd970217e2c1f40c58ef151047
Richell Bonfield <richell.bonfield12@gmail.com>
2019-10-07 22:14:26 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -2,7 +2,8 @@ class BankAccount
   attr_accessor :name, :balance, :status 
   
   def initialize(name, balance = 1000, status = "open")
-    @name = name
+    @name = name 
+    @balance = balance
   end
 
 end
