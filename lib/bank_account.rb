442c32c981dfa2fa3831c43c0f8f5fbdfa260ab2
Richell Bonfield <richell.bonfield12@gmail.com>
2019-10-07 22:23:59 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -15,5 +15,14 @@ class BankAccount
   def display_balance
     "Your balance is $#{balance}."
   end
+  
+  def close_account
+    self.status = "closed"
+  end
+
+  def valid?
+    balance > 0 && status == "open"
+  end
+
     
 end
